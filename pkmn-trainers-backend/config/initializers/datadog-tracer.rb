d96986ef59549a862156e334c002ded20eb64e06
David <davidlago104@gmail.com>
2022-03-24 14:18:36 -0400

    changed ingestion rate to 100% for datadog

--- pkmn-trainers-backend/config/initializers/datadog-tracer.rb
+++ pkmn-trainers-backend/config/initializers/datadog-tracer.rb
@@ -1,2 +1,12 @@
+require 'ddtrace'
+
 Datadog.configure do |c|
+c.tracer sampler: Datadog::PrioritySampler.new(
+post_sampler: Datadog::Sampling::RuleSampler.new(
+    [
+        # Sample all 'pkmn_trainers' traces at 100.00%:
+        Datadog::Sampling::SimpleRule.new(service: 'pkmn_trainers', sample_rate: 1.0000)
+    ]
+)
+)
 end
